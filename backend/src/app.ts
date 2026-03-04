b6426672dce90af6a81f745145ee4e3c514dd019
robertopimentadev <pimentadevapple@gmail.com>
2026-03-04 20:17:41 -0300

    install zod and create schema POST and UPDATE

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,14 +7,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Rota básica para teste
 app.get("/home", (req, res) => {
     return res.json({
         message: "API is running 🚀",
     });
 });
 
-// Rotas principais
 app.use("/password-cards", passwordCardRoutes);
 
 export default app;
